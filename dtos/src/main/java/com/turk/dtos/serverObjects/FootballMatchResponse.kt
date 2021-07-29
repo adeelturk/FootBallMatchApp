a123c08cba6ae710a389f36db7a5ea6cf8d8a320
Adeel turk <adeelturk1991@gmail.com>
2021-07-29 12:04:17 +0400

    -removed Extra Comments

--- dtos/src/main/java/com/turk/dtos/serverObjects/FootballMatchResponse.kt
+++ dtos/src/main/java/com/turk/dtos/serverObjects/FootballMatchResponse.kt
@@ -8,11 +8,4 @@ data class FootballMatchResponse(val Team_A:String,
                                  val Date:String,
 )
 
-/*
 
-"Team_A": "FC Barcelona",
-"Team_B": "Chelsea",
-"Score": "2-0",
-"link_A": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnVkgyvVxOrIGUfaoGPOQPbXKzQUKz7faW71gC7nnI_clFEPbQ81EDQ5T575enZ1Ea5PA&usqp=CAU",
-"link_B": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnVkgyvVxOrIGUfaoGPOQPbXKzQUKz7faW71gC7nnI_clFEPbQ81EDQ5T575enZ1Ea5PA&usqp=CAU",
-"Date": "25 July 2021 21:00"*/
